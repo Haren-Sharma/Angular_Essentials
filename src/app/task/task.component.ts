7e55390b166e80a69a6720433552af57af64d6f1
Haren-Sharma <harensharma33@gmail.com>
2026-05-11 13:31:13 +0530

    Added Users,Tasks and Task Components and some basic flows

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from './task.model';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+  @Input({required:true}) task!:Task;
+  @Output() completed=new EventEmitter();
+  
+  completeTask(){
+    this.completed.emit(this.task.id);
+  }
+
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, computed, EventEmitter, input, Input, Output, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy.users';
+import { User } from './user.model';
+import { NgClass } from "@angular/common";
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({required:true}) user!:User;
+  @Input({required:true}) selected!:boolean;
+
+  @Output() select=new EventEmitter();
+
+  get imagePath(){
+    return  'assets/users/'+this.user.avatar   
+  }
+
+  onSelectedUser(){
+    this.select.emit(this.user.id);
+  }
+}
