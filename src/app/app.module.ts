3fb8e76adb33ed8805ca0960bec03e66ab40d4d6
Haren-Sharma <harensharma33@gmail.com>
2026-05-26 15:53:08 +0530

    multiple modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,17 @@ import { AppComponent } from './app.component';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { CardComponent } from './shared/card/card.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UserComponent, 
-    TasksComponent,
-    CardComponent,
-    TaskComponent,
-    NewTaskComponent,
-], // here we declare all the module-based components that we are going to use
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+  ], // here we declare all the module-based components that we are going to use
   bootstrap: [AppComponent], // first component that will be bootstraped
-  imports: [
-    BrowserModule,
-    RouterOutlet,
-    FormsModule
-  ], // NgModules (and standalone components/directives/pipes) to make available in this module
+  imports: [BrowserModule,SharedModule,RouterOutlet,TasksModule], // NgModules (and standalone components/directives/pipes) to make available in this module
 })
 export class AppModule {}
